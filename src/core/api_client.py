f1a9c376e7d7530458dbc3a5d6abce9331458af3
Sepehr <sepehr.shirkhanlou@yahoo.com>
2023-08-14 18:23:20 +0330

    Implement Dashboards Interface

--- src/core/api_client.py
+++ src/core/api_client.py
@@ -3,6 +3,8 @@ from httpx import Auth
 from httpx import Response
 from httpx import URL
 
+from .interfaces.dashboards_interface import DashBoardsInterface
+
 
 class ApiClient:
     def __init__(self, base_url: URL, auth: Auth, organization_id: int = 1):
@@ -17,7 +19,11 @@ class ApiClient:
             }
         )
 
+    @property
+    def dashboards(self) -> DashBoardsInterface:
+        dashboards_interface = DashBoardsInterface(http_client=self.__http_client)
+        return dashboards_interface
+
     @staticmethod
     async def __raise_on_4xx_5xx(response: Response):
         response.raise_for_status()
-
---
+++ src/core/interfaces/dashboards_interface.py
@@ -0,0 +1,125 @@
+from typing import AsyncIterable
+
+from httpx import AsyncClient
+
+from .base_interface import BaseInterface
+
+
+class Dashboard:
+    data: dict
+
+    def __init__(self, data: dict):
+        self.data = data
+
+    def __str__(self):
+        return str(self.title)
+
+    def __repr__(self):
+        return repr(self.data)
+
+    @property
+    def dashboard_uid(self) -> str:
+        return self.data["dashboard"]["uid"]
+
+    @property
+    def folder_id(self) -> str:
+        return self.data["meta"]["folderId"]
+
+    @property
+    def folder_uid(self) -> str:
+        return self.data["meta"]["folderUid"]
+
+    @property
+    def title(self) -> str:
+        return self.data["dashboard"]["title"]
+
+    @property
+    def striped(self) -> dict[str, dict]:
+        # TODO: this function was made in hurry. most certainly needs a rewrite.
+
+        data = self.data
+        striped_data = dict()
+
+        # Filter keys to be generated again by grafana
+        unneeded_keys = [
+            "id",
+            # "uid",
+            "links",
+            "version",
+        ]
+        striped_data["dashboard"] = {k: v for (k, v) in data["dashboard"].items() if k not in unneeded_keys}
+
+        # Filter keys to be generated again by grafana
+        unneeded_keys = [
+            "url",
+            "expires",
+            "created",
+            "updated",
+            "version",
+            "folderId",
+            "folderUid",
+            "folderUrl",
+            "provisioned",
+            "provisionedExternalId",
+            "publicDashboardAccessToken",
+            "publicDashboardUid",
+            "publicDashboardEnabled",
+        ]
+        striped_data["meta"] = {k: v for (k, v) in data["meta"].items() if k not in unneeded_keys}
+        return striped_data
+
+
+class DashBoardsInterface(BaseInterface):
+    def __init__(
+            self,
+            http_client: AsyncClient,
+            folder_id: int | None = None,
+    ):
+        super().__init__(http_client)
+        self.folder_id = folder_id
+
+    async def get_all_dashboards(
+            self,
+            filter_folder_id: list[str | int] | None = None,
+    ) -> AsyncIterable[Dashboard]:
+        if filter_folder_id is None:
+            if self.folder_id is None:
+                filter_folder_id = []
+            else:
+                filter_folder_id = [self.folder_id]
+        params = {"type": "dash-db", "folderIds": filter_folder_id}
+        r = await self._http_client.get("/api/search", params=params)
+        for dashboard in r.json():
+            yield await self.get_dashboard(uid=dashboard["uid"])
+
+    async def get_dashboard(
+            self,
+            uid: str,
+    ) -> Dashboard:
+        r = await self._http_client.get(f"/api/dashboards/uid/{uid}")
+        return Dashboard(data=r.json())
+
+    async def create_dashboard(
+            self,
+            dashboard: Dashboard,
+            folder_uid: str | None = None,
+    ) -> Dashboard:
+        json_payload = {
+            **dashboard.striped,
+            "folderUid": dashboard.folder_uid,
+            "overwrite": True,
+        }
+        if folder_uid is not None:
+            json_payload["folderUid"] = folder_uid
+        headers = {
+            **self._http_client.headers,
+            "Content-Type": "application/json",
+        }
+        r = await self._http_client.post(
+            url="/api/dashboards/db/",
+            json=json_payload,
+            headers=headers,
+        )
+        new_uid = r.json()["uid"]
+        new_dashboard = await self.get_dashboard(uid=new_uid)
+        return new_dashboard

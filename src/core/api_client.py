e70feabd42f8d2f88499578262fa048782d6d2a0
s.shirkhanlou <s.shirkhanlou@asax.ir>
2023-08-14 18:14:40 +0330

    Handle 4xx and 5xx HTTP errors

--- src/core/api_client.py
+++ src/core/api_client.py
@@ -1,5 +1,6 @@
 from httpx import AsyncClient
 from httpx import Auth
+from httpx import Response
 from httpx import URL
 
 
@@ -8,7 +9,15 @@ class ApiClient:
         self.__http_client = AsyncClient(
             base_url=base_url,
             auth=auth,
+            event_hooks={
+                "response": [self.__raise_on_4xx_5xx],
+            },
             headers={
                 "X-Grafana-Org-Id": str(organization_id)
             }
         )
+
+    @staticmethod
+    async def __raise_on_4xx_5xx(response: Response):
+        response.raise_for_status()
+
